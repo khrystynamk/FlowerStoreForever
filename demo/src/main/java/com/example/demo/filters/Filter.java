384b04453682550efd869ac4f382a9f09bf4b184
khrystynamk <116157359+khrystynamk@users.noreply.github.com>
2023-12-25 18:39:01 +0200

    Delete directory

--- demo/src/main/java/com/example/demo/filters/Filter.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.filters;
-import com.example.demo.flower.Item;
-
-public interface Filter {
-    boolean match(Item item);
-}
--- demo/src/main/java/com/example/demo/flower/FlowerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.flower;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlowerRepository extends JpaRepository<Flower, Integer> {
-    
-}
